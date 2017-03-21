f21499d016b9987eae60d70af770acd858ccd7b2
Tal Yerushalmi <tal.y3rushalmi@gmail.com>
2017-03-21 22:18:03 +0200

    Changed names writted wroneg (ISE17 -> ISE172)

--- ISE172_project/Properties/AssemblyInfo.cs
+++ ISE172_project/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ISE17_project")]
+[assembly: AssemblyTitle("ISE172_project")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ISE17_project")]
+[assembly: AssemblyProduct("ISE172_project")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
