c2a0fb97ad70c99db09de4d8d195cafde21a4972
Tal Yerushalmi <tal.y3rushalmi@gmail.com>
2017-03-21 14:52:36 +0200

    Implemented a simple interpreter to read the arguments, with awesome ASCII art (as a bonus)

---
+++ ISE17_project/AsciiSilverTongue.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program
+{
+    class AsciiSilverTongue
+    {
+
+        public AsciiSilverTongue()
+        {
+            // a default constructor
+        }
+
+        public void PrintWelcome()
+        {
+            string data = @"    ___    __                        ______               __   
+   /   |  / /___ _____              /_  __/________ _____/ /__ 
+  / /| | / / __ `/ __ \   ______     / / / ___/ __ `/ __  / _ \
+ / ___ |/ / /_/ / /_/ /  /_____/    / / / /  / /_/ / /_/ /  __/
+/_/  |_/_/\__, /\____/             /_/ /_/   \__,_/\__,_/\___/ 
+         /____/                                                ";
+
+            Console.WriteLine(data);
+            Console.WriteLine("\nBy Tom Marzea, Roee Groiser and Tal Yerushalmi\n");
+        }
+
+        public void PrintGoodbye()
+        {
+            string data = @"   ______                ____               __
+  / ____/___  ____  ____/ / /_  __  _____  / /
+ / / __/ __ \/ __ \/ __  / __ \/ / / / _ \/ / 
+/ /_/ / /_/ / /_/ / /_/ / /_/ / /_/ /  __/_/  
+\____/\____/\____/\__,_/_.___/\__, /\___(_)   
+                             /____/           ";
+
+            Console.WriteLine(data);
+            Console.WriteLine("\nThank you for using Algo-Trade\n");
+        }
+
+        public void PrintMenu()
+        {
+            Console.WriteLine("What would you like to do?");
+            Console.WriteLine("menu - print this menu");
+            Console.WriteLine("hello [NAME] - say Hello to {name}!");
+            Console.WriteLine("exit - Exit the program");
+        }
+
+        public string ReadLine()
+        {
+            Console.Write("\n> ");
+            return Console.ReadLine();
+        }
+
+    }
+}
---
+++ ISE17_project/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program
+{
+    class Program
+    {
+
+        static AsciiSilverTongue ast = new AsciiSilverTongue();
+
+        public static void HandleInput(string input)
+        {
+            switch (input)
+            {
+                case "menu":
+                    ast.PrintMenu();
+                    break;
+                default:
+                    if (input.Length > 6 && input.IndexOf("hello ") == 0)
+                        Console.WriteLine("Hello " + input.Substring(6));
+                    else
+                        Console.WriteLine("Wrong format - please check your input");
+                    break;
+            }
+            
+        }
+
+        static void Main(string[] args)
+        {
+            ast.PrintWelcome();
+            ast.PrintMenu();
+
+            string input = ast.ReadLine();
+
+            while (!input.Equals("exit"))
+            {
+                HandleInput(input);
+                input = ast.ReadLine();
+            }
+
+            // "exit" was recieved as input - print Goodbye and exit program
+            ast.PrintGoodbye();
+        }
+    }
+}
