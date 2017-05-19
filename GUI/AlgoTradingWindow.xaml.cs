c44bb15b0e27e71081c3e71805ab2c74edfaa377
Tal Yerushalmi <tal.y3rushalmi@gmail.com>
2017-05-19 03:16:36 +0300

    Added most (if not all) User Controls, finished Buy and Sell UC's

--- GUI/AlgoTradingWindow.xaml.cs
+++ GUI/AlgoTradingWindow.xaml.cs
@@ -25,12 +25,6 @@ namespace GUI
             InitializeComponent();
         }
 
-        private void buttonInformation_Click(object sender, RoutedEventArgs e)
-        {
-            UserControlInfo userControlInfo = new UserControlInfo();
-            this.ContentControl.Content = userControlInfo;
-        }
-
         public void ExitProgram()
         {
             Application.Current.Shutdown();
@@ -46,6 +40,42 @@ namespace GUI
             MessageBoxResult messageBoxResult = System.Windows.MessageBox.Show("Are you sure you want to exit?", "Exit Algo-Trading", System.Windows.MessageBoxButton.YesNo);
             if (messageBoxResult == MessageBoxResult.Yes)
                 ExitProgram();
+        }
+
+        private void buttonInformation_Click(object sender, RoutedEventArgs e)
+        {
+            UserControlInfo userControlInfo = new UserControlInfo();
+            this.ContentControl.Content = userControlInfo;
+        }
+
+        private void buttonBuy_Click(object sender, RoutedEventArgs e)
+        {
+            UserControlBuy userControlBuy = new UserControlBuy();
+            this.ContentControl.Content = userControlBuy;
+        }
+
+        private void buttonSell_Click(object sender, RoutedEventArgs e)
+        {
+            UserControlSell userControlSell = new UserControlSell();
+            this.ContentControl.Content = userControlSell;
+        }
+
+        private void buttonSearch_Click(object sender, RoutedEventArgs e)
+        {
+            UserControlSearch userControlSearch = new UserControlSearch();
+            this.ContentControl.Content = userControlSearch;
+        }
+
+        private void buttonSessionHistory_Click(object sender, RoutedEventArgs e)
+        {
+            UserControlSessionHistory userControlSessionHistory = new UserControlSessionHistory();
+            this.ContentControl.Content = userControlSessionHistory;
+        }
+
+        private void buttonActiveRequests_Click(object sender, RoutedEventArgs e)
+        {
+            UserControlActiveRequests userControlActiveRequests = new UserControlActiveRequests();
+            this.ContentControl.Content = userControlActiveRequests;
         }
     }
 }
